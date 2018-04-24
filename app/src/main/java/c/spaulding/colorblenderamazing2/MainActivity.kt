2c0793b2f2424a1e7b03b5856410a951cd1ad158
Aguiardavidm <Aguiardavidm@gmail.com>
2018-04-24 14:47:15 -0600

    Done without intents

--- app/src/main/java/c/spaulding/colorblenderamazing2/MainActivity.kt
+++ app/src/main/java/c/spaulding/colorblenderamazing2/MainActivity.kt
@@ -1,24 +1,47 @@
 package c.spaulding.colorblenderamazing2
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SeekBar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val alpha = 255
+    private val hundred = 100
+    var redOne = 0
+    var blueOne = 0
+    var greenOne = 0
+    var redTwo = 255
+    var blueTwo = 255
+    var greenTwo = 255
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        colorMixBar.max = hundred
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        colorView.setBackgroundColor(Color.argb(alpha,redOne,blueOne,greenOne))
+
+        colorMixBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                when (colorMixBar.progress) {
+                    100 -> colorView.setBackgroundColor(Color.argb(alpha, redTwo, greenTwo, blueTwo))
+                    0 -> colorView.setBackgroundColor(Color.argb(alpha, redOne, greenOne, blueOne))
+                    else -> colorView.setBackgroundColor(Color.argb(alpha,
+                            ((redTwo*colorMixBar.progress/100)+(redOne*(100-colorMixBar.progress)/100)),
+                            ((greenTwo*colorMixBar.progress/100)+(greenOne*(100-colorMixBar.progress)/100)),
+                            ((blueTwo*colorMixBar.progress/100)+(blueOne*(100-colorMixBar.progress)/100))))
+                }
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
