0acf35a97ce80d2bd30f7985aa8199c56dcdac45
Aguiardavidm <Aguiardavidm@gmail.com>
2018-05-05 10:20:22 -0600

    Broken Stuff!!!!!!!

--- app/src/main/java/c/spaulding/colorblenderamazing2/MainActivity.kt
+++ app/src/main/java/c/spaulding/colorblenderamazing2/MainActivity.kt
@@ -1,11 +1,14 @@
 package c.spaulding.colorblenderamazing2
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.SeekBar
@@ -29,12 +32,21 @@ class MainActivity : AppCompatActivity() {
         colorMixBar.max = hundred
 
         val sharedPreferences: SharedPreferences = this.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+
         setBackgroundWSharedPref(sharedPreferences)
+        getNewColorsWithIntents()
+        saveColorsInSharedPrefs(sharedPreferences)
 
         colorView.setBackgroundColor(Color.argb(alpha,redOne,blueOne,greenOne))
 
         setBackgroundOnSlider()
 
+        colorOne.setOnClickListener{
+            openColorPickerApp(12, 1)
+        }
+        colorTwo.setOnClickListener {
+            openColorPickerApp(12,2)
+        }
     }
 
     private fun saveColorsInSharedPrefs(sharedPreferences: SharedPreferences){
@@ -71,5 +83,41 @@ class MainActivity : AppCompatActivity() {
         redTwo = sharedPreferences.getInt("redTwo", 255)
         blueTwo = sharedPreferences.getInt("blueTwo", 255)
         greenTwo = sharedPreferences.getInt("greenTwo", 255)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        saveColorsInSharedPrefs(sharedPreferences)
+    }
+
+    fun openColorPickerApp( requestCode: Int, side : Int){
+        val launchColorPicker = packageManager.getLaunchIntentForPackage("c.ebookfrenzy.colorpicker")  as Intent
+        if (launchColorPicker != null) {
+            launchColorPicker.putExtra("side", side)
+            startActivityForResult(launchColorPicker, requestCode)
+        } else {
+            // Bring user to the market or let them choose an app?
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.data = Uri.parse("market://details?id=" + packageName)
+            startActivity(intent);
+        }
+    }
+
+    fun getNewColorsWithIntents() {
+        if(intent == null){
+            Log.i("nullIntent", "NO INTENTS PASSED ~HULK")
+        }
+        else if(intent.getIntExtra("side", 0) == 1){
+            redOne = intent.getIntExtra("red", 0)
+            blueOne = intent.getIntExtra("blue", 0)
+            greenOne = intent.getIntExtra("green", 0)
+        }
+        else if (intent.getIntExtra("side", 0) == 2){
+            redTwo = intent.getIntExtra("red", 0)
+            blueTwo = intent.getIntExtra("blue", 0)
+            greenTwo = intent.getIntExtra("green", 0)
+        }
     }
 }
